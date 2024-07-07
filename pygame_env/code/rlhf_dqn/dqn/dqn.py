ae94ae999d548b637deff682a320abc92d463638
Marvin <lim343@purdue.edu>
2024-07-07 17:58:45 +0800

    Store NN as Agent attributes. Add cuda.

--- pygame_env/code/rlhf_dqn/dqn/dqn.py
+++ pygame_env/code/rlhf_dqn/dqn/dqn.py
@@ -1,9 +1,7 @@
-# https://www.youtube.com/watch?v=EUrWGTCGzlA&t=1088s&ab_channel=JohnnyCode
+##############################################################################
+# Store NN as Agent attributes. Add cuda. 
+##############################################################################
 
-##############################################################################
-# Original implementation 
-##############################################################################
-  
 from environment import Grid
 import numpy as np
 import matplotlib.pyplot as plt
@@ -15,12 +13,12 @@ import torch.nn.functional as F
 
 # Define model
 class DQN(nn.Module):
-    def __init__(self, in_states, h1_nodes, out_actions):
+    def __init__(self, in_states, out_actions):
         super().__init__()
 
         # Define network layers
-        self.fc1 = nn.Linear(in_states, h1_nodes, dtype=torch.float64)   # first fully connected layer
-        self.out = nn.Linear(h1_nodes, out_actions, dtype=torch.float64) # ouptut layer w
+        self.fc1 = nn.Linear(in_states, 128, dtype=torch.float64)   # first fully connected layer
+        self.out = nn.Linear(128, out_actions, dtype=torch.float64) # ouptut layer w
 
     def forward(self, x):
         x = F.relu(self.fc1(x)) # Apply rectified linear unit (ReLU) activation
@@ -41,44 +39,32 @@ class ReplayMemory():
     def __len__(self):
         return len(self.memory)
 
-# FrozeLake Deep Q-Learning
 class Agent():
-    # Hyperparameters (adjustable)
-    learning_rate_a = 0.001         # learning rate (alpha)
-    discount_factor_g = 0.9         # discount rate (gamma)    
-    network_sync_rate = 10          # number of steps the agent takes before syncing the policy and target network
-    replay_memory_size = 1000       # size of replay memory
-    mini_batch_size = 32            # size of the training data set sampled from the replay memory
+    def __init__(self, env):
+        self.env = env
+        self.num_states = env.obs_space
+        self.num_actions = len(env.action_space)
 
-    # Neural Network
-    loss_fn = nn.MSELoss()          # NN Loss function. MSE=Mean Squared Error can be swapped to something else.
-    optimizer = None                # NN Optimizer. Initialize later.
+        # Hyperparameters (adjustable)
+        self.learning_rate_a = 0.001         # learning rate (alpha)
+        self.discount_factor_g = 0.9         # discount rate (gamma)    
+        self.network_sync_rate = 10          # number of steps the agent takes before syncing the policy and target network
+        self.replay_memory_size = 1000       # size of replay memory
+        self.mini_batch_size = 32            # size of the training data set sampled from the replay memory
+        self.epsilon = 1                     # 1 = 100% random actions
 
-    ACTIONS = ['U','D','L','R']     
+        self.memory = ReplayMemory(self.replay_memory_size)
 
-    # Train the FrozeLake environment
-    def train(self, episodes, maze, render=False):
-        # Create FrozenLake instance
-        env = Grid(maze=maze, show_render=render)
-        num_states = env.obs_space
-        num_actions = len(env.action_space)
-        
-        epsilon = 1 # 1 = 100% random actions
-        memory = ReplayMemory(self.replay_memory_size)
+        # Neural Network
+        self.policy_dqn = DQN(in_states=self.num_states, out_actions=self.num_actions)
+        self.target_dqn = DQN(in_states=self.num_states, out_actions=self.num_actions)
+        self.policy_dqn.cuda()
+        self.target_dqn.cuda()
+        self.target_dqn.load_state_dict(self.policy_dqn.state_dict()) # Make the target and policy networks the same (copy weights/biases from one network to the other)
+        self.loss_fn = nn.MSELoss()          # NN Loss function. MSE=Mean Squared Error can be swapped to something else.
+        self.optimizer = torch.optim.Adam(self.policy_dqn.parameters(), lr=self.learning_rate_a)
 
-        # Create policy and target network. Number of nodes in the hidden layer can be adjusted.
-        policy_dqn = DQN(in_states=num_states, h1_nodes=32, out_actions=num_actions)
-        target_dqn = DQN(in_states=num_states, h1_nodes=32, out_actions=num_actions)
- 
-        # Make the target and policy networks the same (copy weights/biases from one network to the other)
-        target_dqn.load_state_dict(policy_dqn.state_dict())
-
-        # print('Policy (random, before training):')
-        # self.print_dqn(policy_dqn)
-
-        # Policy network optimizer. "Adam" optimizer can be swapped to something else. 
-        self.optimizer = torch.optim.Adam(policy_dqn.parameters(), lr=self.learning_rate_a)
-
+    def train(self, episodes):
         # List to keep track of rewards collected per episode. Initialize list to 0's.
         rewards_per_episode = np.zeros(episodes)
 
@@ -86,58 +72,56 @@ class Agent():
         epsilon_history = []
 
         # Track number of steps taken. Used for syncing policy => target network.
-        step_count=0
+        step_count = 0
             
         for i in range(episodes):
             print(i)
-            state = env.reset()  # Initialize to state 0
+            state = self.env.reset()     # Initialize to state 0
             terminated = False      # True when agent falls in hole or reached goal
             truncated = False       # True when agent takes more than 200 actions    
 
             # Agent navigates map until it falls into hole/reaches goal (terminated), or has taken 200 actions (truncated).
             while(not terminated and not truncated):
-
                 # Select action based on epsilon-greedy
-                if random.random() < epsilon:
-                    action = random.choice(env.action_space) # select random action
+                if random.random() < self.epsilon:
+                    action = random.choice(self.env.action_space) # select random action
                 else:
                     # select best action            
                     with torch.no_grad():
-                        action = policy_dqn(torch.from_numpy(state)).argmax().item()
+                        action = self.policy_dqn(torch.from_numpy(state).cuda()).detach().argmax().item()
 
                 # Execute action
-                new_state,reward,terminated,truncated = env.step(action)
+                new_state, reward, terminated, truncated = self.env.step(action)
 
                 # Save experience into memory
-                memory.append((state, action, new_state, reward, terminated)) 
+                self.memory.append((state, action, new_state, reward, terminated)) 
 
                 # Move to the next state
                 state = new_state
 
                 # Increment step counter
-                step_count+=1
+                step_count += 1
 
             # Keep track of the rewards collected per episode.
             if reward == 1:
                 rewards_per_episode[i] = 1
 
             # Check if enough experience has been collected and if at least 1 reward has been collected
-            if len(memory)>self.mini_batch_size and np.sum(rewards_per_episode)>0:
-                mini_batch = memory.sample(self.mini_batch_size)
-                self.optimize(mini_batch, policy_dqn, target_dqn)        
+            if len(self.memory) > self.mini_batch_size and np.sum(rewards_per_episode) > 0:
+                mini_batch = self.memory.sample(self.mini_batch_size)
+                self.optimize(mini_batch)        
 
                 # Decay epsilon
-                epsilon = max(epsilon - 1/episodes, 0)
-                epsilon_history.append(epsilon)
+                self.epsilon = max(self.epsilon - 1/episodes, 0)
+                epsilon_history.append(self.epsilon)
 
                 # Copy policy network to target network after a certain number of steps
                 if step_count > self.network_sync_rate:
-                    target_dqn.load_state_dict(policy_dqn.state_dict())
-                    step_count=0
-
+                    self.target_dqn.load_state_dict(self.policy_dqn.state_dict())
+                    step_count = 0
 
         # Save policy
-        torch.save(policy_dqn.state_dict(), "model.pt")
+        torch.save(self.policy_dqn.state_dict(), "model.pt")
 
         # Create new graph 
         plt.figure(1)
@@ -157,10 +141,10 @@ class Agent():
         plt.savefig('graph.png')
 
     # Optimize policy network
-    def optimize(self, mini_batch, policy_dqn, target_dqn):
+    def optimize(self, mini_batch):
 
         # Get number of input nodes
-        num_states = policy_dqn.fc1.in_features
+        num_states = self.policy_dqn.fc1.in_features
 
         current_q_list = []
         target_q_list = []
@@ -172,16 +156,14 @@ class Agent():
                 # When in a terminated state, target q value should be set to the reward.
                 target = torch.FloatTensor([reward])
             else:
-                # Calculate target q value 
-                with torch.no_grad():
-                    target = reward + self.discount_factor_g * target_dqn(torch.from_numpy(new_state)).max()
+                target = reward + self.discount_factor_g * self.target_dqn(torch.from_numpy(new_state).cuda()).max()
 
             # Get the current set of Q values
-            current_q = policy_dqn(torch.from_numpy(state))
+            current_q = self.policy_dqn(torch.from_numpy(state).cuda())
             current_q_list.append(current_q)
 
             # Get the target set of Q values
-            target_q = target_dqn(torch.from_numpy(state)) 
+            target_q = self.target_dqn(torch.from_numpy(state).cuda()) 
             # Adjust the specific action to the target that was just calculated
             target_q[action] = target
             target_q_list.append(target_q)
@@ -196,23 +178,17 @@ class Agent():
 
 
     # Run the FrozeLake environment with the learned policy
-    def test(self, episodes, maze):
-        # Create FrozenLake instance
-        env = Grid(maze=maze, show_render=True)
-        num_states = env.obs_space
-        num_actions = len(env.action_space)
+    def test(self, episodes):
+        self.env.show_render = True
 
         # Load learned policy
-        policy_dqn = DQN(in_states=num_states, h1_nodes=32, out_actions=num_actions) 
+        policy_dqn = DQN(in_states=self.num_states, out_actions=self.num_actions) 
         policy_dqn.load_state_dict(torch.load("model.pt"))
         policy_dqn.eval()    # switch model to evaluation mode
 
-        # print('Policy (trained):')
-        # self.print_dqn(policy_dqn)
-
         for i in range(episodes):
             print(i)
-            state = env.reset()     # Initialize to state 0
+            state = self.env.reset()     # Initialize to state 0
             terminated = False      # True when agent falls in hole or reached goal
             truncated = False       # True when agent takes more than 200 actions            
 
@@ -223,33 +199,12 @@ class Agent():
                     action = policy_dqn(torch.from_numpy(state)).argmax().item()
 
                 # Execute action
-                state,reward,terminated,truncated = env.step(action)
-
-    # Print DQN: state, best action, q values
-    def print_dqn(self, dqn):
-        # Get number of input nodes
-        num_states = dqn.fc1.in_features
-
-        # Loop each state and print policy to console
-        for s in range(num_states):
-            #  Format q values for printing
-            q_values = ''
-            for q in dqn(self.state_to_dqn_input(s, num_states)).tolist():
-                q_values += "{:+.2f}".format(q)+' '  # Concatenate q values, format to 2 decimals
-            q_values=q_values.rstrip()              # Remove space at the end
-
-            # Map the best action to L D R U
-            best_action = self.ACTIONS[dqn(self.state_to_dqn_input(s, num_states)).argmax()]
-
-            # Print policy in the format of: state, action, q values
-            # The printed layout matches the FrozenLake map.
-            print(f'{s:02},{best_action},[{q_values}]', end=' ')         
-            if (s+1)%4==0:
-                print() # Print a newline every 4 states
+                state, reward, terminated, truncated = self.env.step(action)
 
 if __name__ == '__main__':
-
+    print("GPU:", torch.cuda.is_available())
     maze = 'maze1'
-    agent = Agent()
-    agent.train(3000, render=False, maze=maze)
-    # agent.test(10, maze=maze)
+    env = Grid(maze=maze, show_render=False)
+    agent = Agent(env)
+    agent.train(3000)
+    agent.test(10)
